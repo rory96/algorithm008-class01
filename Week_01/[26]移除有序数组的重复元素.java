3ec716989685fe0d08e22249ce583750afeb21ed
雷永麒 <leiyongqi1026@gmail.com>
2020-04-16 22:47:53 +0900

    Update [26]移除有序数组的重复元素.java
    更改注释

--- Week_01/[26]移除有序数组的重复元素.java
+++ Week_01/[26]移除有序数组的重复元素.java
@@ -5,7 +5,7 @@ package com.keanu.algorithm.leetcode.easy;
  */
 public class RemoveDuplicates {
     public int removeDuplicates(int[] nums) {
-        int i = 1; // 这里应该是0，但是提交代码没有报错。考虑到[]数组元素个数为0的情况。
+        int i = 1; // 这里应该是0，考虑到元素为空的情况，会返回1，但是提交之后测试的case并没有失败。
         for (int n : nums) {
             if (n > nums[i - 1]) { // 如果 i = 0 时，这里的判断应该是 if (i == 0 || n > nums[i - 1])
                 nums[i++] = n;
